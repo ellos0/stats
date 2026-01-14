eb3c85f8714dccf6f350da0afe334ff7ef0feed6
ellos <elliottmv0@gmail.com>
2026-01-14 06:52:46 -0800

    added pies.r

--- src/pies.r
+++ src/pies.r
@@ -1,5 +1,5 @@
 d1 <- c(60,120,20,50,150)
 l1 <- c("Highest", "Medium", "Lowest", "Personal", "Spam")
 
-pie(dq, l1=piepercent,
+pie(d1, labels=l1,
         main = "Office Manager Email Priority Chart")
